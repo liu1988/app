429b8c2b7252b42fad3f6b5ff5dc7c22a2290a44
liuxiaolong <234119939@qq.com>
2019-10-28 21:52:00 +0800

    聊天列表与对话框

--- App.vue
+++ App.vue
@@ -14,6 +14,15 @@
 	}
 </script>
 
+
 <style>
 	/*每个页面公共css */
+	page {
+		min-height: 100%;
+		background-color: #ede8ee;
+		height: 100%;
+		font-size: 32px;
+		line-height: 1.6;
+	}
 </style>
+
---
+++ pages/news/ChatList.vue
@@ -0,0 +1,36 @@
+<template>
+	<view>
+		<uni-list>
+		    <uni-list-item class="icon-img" title="标题文字" :show-arrow="false" note="描述信息" thumb="../../static/index/fate/1.jpg"></uni-list-item>
+		    <uni-list-item title="标题文字" :show-arrow="false" note="描述信息" thumb="../../static/index/fate/2.jpg"></uni-list-item>
+		    <uni-list-item title="标题文字" :show-arrow="false" :show-badge="true"  note="描述信息" thumb="../../static/index/fate/3.jpg"></uni-list-item>
+		    <uni-list-item title="禁用状态" :show-arrow="false" :disabled="false" :show-badge="true"  note="描述信息" thumb="../../static/index/fate/4.jpg"></uni-list-item>
+			<uni-list-item title="禁用状态" :show-arrow="false" :disabled="false" :show-badge="true"  note="描述信息" thumb="../../static/index/fate/5.jpg"></uni-list-item>
+		</uni-list>
+	</view>
+</template>
+
+<script>
+	
+	import uniList from "@dcloudio/uni-ui/lib/uni-list/uni-list.vue"
+	import uniListItem from "@dcloudio/uni-ui/lib/uni-list-item/uni-list-item.vue"
+	export default {
+	    components: {uniList,uniListItem},
+		
+		data() {
+			return {
+				
+			}
+		},
+		methods: {
+			
+		}
+	}
+</script>
+
+<style>
+/* .icon-img{
+	    height: 40px;
+	    width: 40px;
+} */
+</style>
